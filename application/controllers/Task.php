b6516e17c3cc95aff503b91594c654e8c777dc8c
Unaib Amir <unaib.webxity@gmail.com>
2019-11-09 13:14:46 +0500

    fixed columns issues

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -1,28 +1,29 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Task extends CI_Controller 
+class Task extends CI_Controller
 {
 
-	public function __construct()
-	{
-		parent::__construct();
-		
-        if(!$this->aauth->is_loggedin())
-			redirect(base_url('/'));
+    public function __construct()
+    {
+        parent::__construct();
+        
+        if (!$this->aauth->is_loggedin()) {
+            redirect(base_url('/'));
+        }
 
-		$this->currentUser = $this->aauth->get_user();
-		$this->currentUserGroup = $this->aauth->get_user_groups();
+        $this->currentUser = $this->aauth->get_user();
+        $this->currentUserGroup = $this->aauth->get_user_groups();
 
         $this->load->helper(array('form', 'url', 'file','directory', 'date'));
-	}
+    }
 
-	public function index()
-	{
+    public function index()
+    {
 
-		//$sql = "SELECT T.*, D.c_name FROM `tasks` AS T LEFT JOIN departments AS D on D.cid = T.department_id ORDER BY T.t_created_at ASC";
-        $view = !empty( $this->input->get('view') ) ? $this->input->get('view') : "daily";
-        $employee_id = !empty( $this->input->get('employee_id') ) ? $this->input->get('employee_id') : false;
+        //$sql = "SELECT T.*, D.c_name FROM `tasks` AS T LEFT JOIN departments AS D on D.cid = T.department_id ORDER BY T.t_created_at ASC";
+        $view = !empty($this->input->get('view')) ? $this->input->get('view') : "daily";
+        $employee_id = !empty($this->input->get('employee_id')) ? $this->input->get('employee_id') : false;
         
         $this->db->select('*');
         $this->db->from('tasks');
@@ -50,41 +51,41 @@ class Task extends CI_Controller
                 break;
         }
 
-        if( $employee_id ) {
+        if ($employee_id) {
             $this->db->where('tasks.assignee', $employee_id);
-		}
-		
-		if($this->currentUserGroup[0]->name == "Employee" ) {
-			$this->db->where('tasks.assignee', $this->currentUser->id );
-		}
+        }
+        
+        if ($this->currentUserGroup[0]->name == "Employee") {
+            $this->db->where('tasks.assignee', $this->currentUser->id);
+        }
 
         $tasks = $this->db->get()->result();
-		
+        
         $data['tasks'] = $tasks;
-		
-		$data['heading1'] = 'Task Listing';
-		$data['nav1'] = $this->currentUserGroup[0]->name;
-		$data['users'] = $this->db->get("aauth_users")->result_array();
+        
+        $data['heading1'] = 'Task Listing';
+        $data['nav1'] = $this->currentUserGroup[0]->name;
+        $data['users'] = $this->db->get("aauth_users")->result_array();
 
-		$data['currentUser'] = $this->currentUser;
-		$data['currentUserGroup'] = $this->currentUserGroup[0]->name;
-		$data['inc_page'] = 'task/list'; // views/display.php page
-		
+        $data['currentUser'] = $this->currentUser;
+        $data['currentUserGroup'] = $this->currentUserGroup[0]->name;
+        $data['inc_page'] = 'task/list'; // views/display.php page
+        
 
         $this->load->view('manager_layout', $data);
     }
 
     public function add()
     {
-    	$this->load->library('form_validation');
+        $this->load->library('form_validation');
 
-    	$data['heading1'] = 'Task from';
-    	$data['nav1'] = $this->currentUserGroup[0]->name;
-    	$data['task_code'] = $this->generateRandomString(4);
-		//select all department
-    	$data['departments'] = $this->getDepartments();
-		//select all employees
-		$data['employees'] = $this->getUsers('Employee');
+        $data['heading1'] = 'Task from';
+        $data['nav1'] = $this->currentUserGroup[0]->name;
+        $data['task_code'] = $this->generateRandomString(4);
+        //select all department
+        $data['departments'] = $this->getDepartments();
+        //select all employees
+        $data['employees'] = $this->getUsers('Employee');
 
 
         //dd(str_pad(555, 4, '0', STR_PAD_LEFT));
@@ -94,21 +95,21 @@ class Task extends CI_Controller
         $last_task_id = str_pad($last_task_id[0]["tid"], 4, '0', STR_PAD_LEFT);
         $data["last_task_id"] = $last_task_id + 1;
 
-		$employee_id = "";
-        if( $this->currentUserGroup[0]->name == "Employee" ) {
+        $employee_id = "";
+        if ($this->currentUserGroup[0]->name == "Employee") {
             $employee_id = $this->currentUserGroup[0]->user_id;
         } else {
             $employee_id = isset($_GET["employee_id"]) && !empty($_GET["employee_id"]) ? $_GET["employee_id"] : "";
         }
 
-        if( !empty( $employee_id ) ) {
-            $data[ 'employee_user' ] = $this->aauth->get_user( $employee_id );
+        if (!empty($employee_id)) {
+            $data[ 'employee_user' ] = $this->aauth->get_user($employee_id);
         }
 
         $data['employee_id'] = $employee_id;
 
-    	$data['currentUser'] = $this->currentUser;
-    	$data['currentUserGroup'] = $this->currentUserGroup[0]->name;
+        $data['currentUser'] = $this->currentUser;
+        $data['currentUserGroup'] = $this->currentUserGroup[0]->name;
 
         $data['inc_page'] = 'task/add'; // views/task/add.php page
         $this->load->view('manager_layout', $data);
@@ -116,13 +117,12 @@ class Task extends CI_Controller
 
     public function save()
     {
-		dd($_POST);
+        
         $file_id = 0;
-        if( isset($_FILES["attachement"]) ) {
-
+        if (isset($_FILES["attachement"])) {
             $upload_path                = "uploads/tasks";
-            if( !is_dir( $upload_path ) ) {
-                mkdir( $upload_path, 0777, true );
+            if (!is_dir($upload_path)) {
+                mkdir($upload_path, 0777, true);
             }
             
             $file                       = array();
@@ -131,8 +131,7 @@ class Task extends CI_Controller
 
             $this->load->library('upload', $config);
 
-            if ( ! $this->upload->do_upload('attachement')) {
-                
+            if (! $this->upload->do_upload('attachement')) {
             } else {
                 $file_data          = $this->upload->data();
                 //dd($file_data);
@@ -147,45 +146,46 @@ class Task extends CI_Controller
             }
         }
 
-		//server validation
-    	$data = array(
-    		't_title'         => $this->input->post('title'),
-    		't_code'          => $this->input->post('code'),
-    		'department_id'   => $this->input->post('department'),
-    		'parent_id'       => $this->input->post('parentId'),
-    		'assignee'        => $this->currentUserGroup[0]->name == "Employee" ? $this->currentUserGroup[0]->user_id : $this->input->post('assignee'),
-    		'reporter'        => $this->input->post('reporter'),
-			'attachment_id'   => $file_id,
-    		't_description'   => $this->input->post('description'),
-    		'start_date'      => date( "Y-m-d", strtotime($this->input->post('start_date')) ),
-    		'end_date'        => date( "Y-m-d", strtotime($this->input->post('end_date')) ),
-    		'created_by'      => (!empty($this->currentUser->id))? $this->currentUser->id: 0
-    	);
+        //server validation
+        $data = array(
+            't_title'         => $this->input->post('title'),
+            't_code'          => $this->input->post('code'),
+            'department_id'   => $this->input->post('department'),
+            'parent_id'       => $this->input->post('parentId'),
+            'assignee'        => $this->currentUserGroup[0]->name == "Employee" ? $this->currentUserGroup[0]->user_id : $this->input->post('assignee'),
+            'reporter'        => $this->input->post('reporter'),
+            'given_by'        => $this->input->post('given_by'),
+            'attachment_id'   => $file_id,
+            't_description'   => $this->input->post('description'),
+            'start_date'      => date("Y-m-d", strtotime($this->input->post('start_date'))),
+            'end_date'        => date("Y-m-d", strtotime($this->input->post('end_date'))),
+            'created_by'      => (!empty($this->currentUser->id))? $this->currentUser->id: 0
+        );
 
-    	$this->db->insert('tasks', $data);
+        $this->db->insert('tasks', $data);
 
-		//get task id and upload files
+        //get task id and upload files
 
-    	redirect(base_url('task/alert'));
+        redirect(base_url('task'));
     }
 
     public function assign()
     {
-    	exit('commming soon!');
+        exit('commming soon!');
     }
 
     public function history()
     {
-    	exit('commming soon!');
+        exit('commming soon!');
     }
 
     public function alert()
     {
-    	$data['nav1'] = 'GEW Employee';
-    	/*$sql = "SELECT T.*, D.c_name FROM `tasks` AS T LEFT JOIN departments AS D on D.cid = T.department_id WHERE T.assignee = ?";
-    	$data['tasks'] = $this->db->query($sql, $this->currentUser->id)->result();*/
+        $data['nav1'] = 'GEW Employee';
+        /*$sql = "SELECT T.*, D.c_name FROM `tasks` AS T LEFT JOIN departments AS D on D.cid = T.department_id WHERE T.assignee = ?";
+        $data['tasks'] = $this->db->query($sql, $this->currentUser->id)->result();*/
 
-        $view = !empty( $this->input->get('view') ) ? $this->input->get('view') : "daily";
+        $view = !empty($this->input->get('view')) ? $this->input->get('view') : "daily";
 
         $this->db->select('*');
         $this->db->from('tasks');
@@ -216,9 +216,8 @@ class Task extends CI_Controller
 
         $tasks = $this->db->get()->result();
         foreach ($tasks as $key => $task) {
-
             $reported = $this->db->query("SELECT * FROM `reports` WHERE task_id ={$task->tid} AND user_id = {$this->currentUser->id} AND DATE(created_at) = CURDATE()")->result_array();
-            if( !empty($reported) && isset($reported[0]) ) {
+            if (!empty($reported) && isset($reported[0])) {
                 $task->reported = true;
             } else {
                 $task->reported = false;
@@ -226,20 +225,19 @@ class Task extends CI_Controller
 
             $this->db->select('*');
             $this->db->from('files');
-            $this->db->where('files.fid', $task->attachment_id );
+            $this->db->where('files.fid', $task->attachment_id);
             $files = $this->db->get()->result_array();
             $task->files = $files;
-
         }
 
         $data['tasks'] = $tasks;
 
-    	$data['heading1'] = 'EMPLOYEE CODE <h5>GEW - '.$this->currentUser->username.'</h5>';
-    	$data['nav1'] = $this->currentUserGroup[0]->name;
+        $data['heading1'] = 'EMPLOYEE CODE <h5>GEW - '.$this->currentUser->username.'</h5>';
+        $data['nav1'] = $this->currentUserGroup[0]->name;
 
-    	$data['currentUser'] = $this->currentUser;
-		$data['currentUserGroup'] = $this->currentUserGroup[0]->name;
-		$data['users'] = $this->db->get("aauth_users")->result_array();
+        $data['currentUser'] = $this->currentUser;
+        $data['currentUserGroup'] = $this->currentUserGroup[0]->name;
+        $data['users'] = $this->db->get("aauth_users")->result_array();
         $data['inc_page'] = 'task/alert'; // views/display.php page
         
 
@@ -249,46 +247,46 @@ class Task extends CI_Controller
 
     public function getDepartments()
     {
-		//$sql = "SELECT * FROM departments WHERE id = ? AND status = ? AND author = ?";
-		//$res = $this->db->query($sql, array(3, 'live', 'Rick'));
-    	$sql = "SELECT * FROM departments WHERE c_status = ?";
-    	$res = $this->db->query($sql, array(1));
+        //$sql = "SELECT * FROM departments WHERE id = ? AND status = ? AND author = ?";
+        //$res = $this->db->query($sql, array(3, 'live', 'Rick'));
+        $sql = "SELECT * FROM departments WHERE c_status = ?";
+        $res = $this->db->query($sql, array(1));
 
-		//$this->db->error();
-    	$res = $res->result();
-    	return $res;
+        //$this->db->error();
+        $res = $res->result();
+        return $res;
     }
 
-    public function getUsers($group){
-    	$sql = "SELECT users.id, users.first_name, users.last_name FROM `aauth_groups` AS grp LEFT JOIN aauth_user_to_group AS grpusr ON grp.id = grpusr.group_id LEFT JOIN aauth_users AS users ON users.id = grpusr.user_id WHERE `name` = ?";
-    	$res = $this->db->query($sql, array($group))->result();
-		//echo $this->db->last_query();
-
-    	return $res;
-    }
-
-    public function generateRandomString($length = 10) 
+    public function getUsers($group)
     {
-		//abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
-    	$characters = '0123456789';
-    	$charactersLength = strlen($characters);
-    	$randomString = '';
-    	for ($i = 0; $i < $length; $i++) {
-    		$randomString .= $characters[rand(0, $charactersLength - 1)];
-    	}
-    	return $randomString;
+        $sql = "SELECT users.id, users.first_name, users.last_name FROM `aauth_groups` AS grp LEFT JOIN aauth_user_to_group AS grpusr ON grp.id = grpusr.group_id LEFT JOIN aauth_users AS users ON users.id = grpusr.user_id WHERE `name` = ?";
+        $res = $this->db->query($sql, array($group))->result();
+        //echo $this->db->last_query();
+
+        return $res;
+    }
+
+    public function generateRandomString($length = 10)
+    {
+        //abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+        $characters = '0123456789';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 
 
     /*****************AJAX Calls****************/
     public function detail($task_id)
     {
-    	if(!empty($task_id))
-    	{
-    		$sql = "SELECT T.*, D.c_name, assignee.first_name as given, reporter.first_name as follow FROM `tasks` AS T LEFT JOIN aauth_users AS assignee ON assignee.id = T.assignee LEFT JOIN aauth_users AS reporter ON reporter.id = T.reporter LEFT JOIN departments AS D ON D.cid = T.department_id WHERE T.tid = ?";
-    		$taskDetail = $this->db->query($sql, $task_id)->row();
+        if (!empty($task_id)) {
+            $sql = "SELECT T.*, D.c_name, assignee.first_name as given, reporter.first_name as follow FROM `tasks` AS T LEFT JOIN aauth_users AS assignee ON assignee.id = T.assignee LEFT JOIN aauth_users AS reporter ON reporter.id = T.reporter LEFT JOIN departments AS D ON D.cid = T.department_id WHERE T.tid = ?";
+            $taskDetail = $this->db->query($sql, $task_id)->row();
 
-    		echo "<div class='row'>
+            echo "<div class='row'>
     		<div class='col-lg-12'>
     		<div class='card'>
     		<div class='card-body'>
@@ -324,7 +322,6 @@ class Task extends CI_Controller
     		</div>
 
     		<button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal' data-whatever='@mdo'>Quick View</button>";
-
-    	}    	
+        }
     }
 }
--- application/views/task/list.php
+++ application/views/task/list.php
@@ -5,105 +5,108 @@
 </div>
 <?php
 $job_types = array(
-	1 => "Daily",
-	2 => "Weekly",
-	3 => "Monthly",
-	4 => "One Time"
+    1 => "Daily",
+    2 => "Weekly",
+    3 => "Monthly",
+    4 => "One Time"
 );
 $job_type = isset($_GET["view"]) ? $_GET["view"] : "daily";
 ?>
 <!-- Dashboard for User -->
 <div class="content">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="card">
-				<div class="card-header">
-					<h5 class="title">Task Listing</h5>
-				</div>
-				<div class="card-body">
-					<nav>
-						<div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
-							<a class="nav-item nav-link <?php echo job_type_state($job_type, "daily"); ?>" id="nav-task-daily" href="<?php echo task_list_link("daily"); ?>"><kbd>Daily</kbd></a>
-							<a class="nav-item nav-link <?php echo job_type_state($job_type, "weekly"); ?>" id="nav-task-weekly" href="<?php echo task_list_link("weekly"); ?>"><kbd>Weekly</kbd></a>
-							<a class="nav-item nav-link <?php echo job_type_state($job_type, "monthly"); ?>" id="nav-task-monthly" href="<?php echo task_list_link("monthly"); ?>"><kbd>Monthly</kbd></a>
-							<a class="nav-item nav-link <?php echo job_type_state($job_type, "one-time"); ?>" id="nav-task-one-time" href="<?php echo task_list_link("one-time"); ?>"><kbd>One Time</kbd></a>
-						</div>
-					</nav>
-					<?php if (!empty($tasks)) : ?>
-						<div class="table-responsive">
-							<table class="table table-bordered table-hover" id="table-list">
-								<thead class=" thead-dark text-primary">
-									<tr>
-										<th>Task Code</th>
-										<th>Ttile</th>
-										<th>Assigned To</th>
-										<th>Given By</th>
-										<th>Department</th>
-										<th>Job Type</th>
-										<th>Follow Up </th>
-										<th>Start Date</th>
-										<th>End Date</th>
-										<th>Status</th>
-										<th>Action</th>
-									</tr>
-								</thead>
-								<tbody>
-									<?php 
-									foreach ($tasks as $task) {
-											$t_given = !empty($task->given_by) ? $task->given_by : $task->created_by;
-											$given_by_key = array_search($t_given, array_column($users, "id"));
-											$assigned_user_key =  array_search($task->assignee, array_column($users, "id"));
-											$start_date = date($this->config->item('date_format'), strtotime($task->start_date));
-											$end_date = date($this->config->item('date_format'), strtotime($task->end_date));
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="title">Task Listing</h5>
+                </div>
+                <div class="card-body">
+                    <nav>
+                        <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                            <a class="nav-item nav-link <?php echo job_type_state($job_type, "daily"); ?>" id="nav-task-daily" href="<?php echo task_list_link("daily"); ?>"><kbd>Daily</kbd></a>
+                            <a class="nav-item nav-link <?php echo job_type_state($job_type, "weekly"); ?>" id="nav-task-weekly" href="<?php echo task_list_link("weekly"); ?>"><kbd>Weekly</kbd></a>
+                            <a class="nav-item nav-link <?php echo job_type_state($job_type, "monthly"); ?>" id="nav-task-monthly" href="<?php echo task_list_link("monthly"); ?>"><kbd>Monthly</kbd></a>
+                            <a class="nav-item nav-link <?php echo job_type_state($job_type, "one-time"); ?>" id="nav-task-one-time" href="<?php echo task_list_link("one-time"); ?>"><kbd>One Time</kbd></a>
+                        </div>
+                    </nav>
+                    <?php if (!empty($tasks)) : ?>
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-hover" id="table-list">
+                                <thead class=" thead-dark text-primary">
+                                    <tr>
+                                        <th>Task Code</th>
+                                        <th>Ttile</th>
+                                        <th>Assigned To</th>
+                                        <th>Given By</th>
+                                        <th>Department</th>
+                                        <th>Job Type</th>
+                                        <th>Follow Up </th>
+                                        <th>Start Date</th>
+                                        <th>End Date</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    foreach ($tasks as $task) {
+                                            $t_given = !empty($task->given_by) ? $task->given_by : $task->created_by;
+                                            $given_by_key = array_search($t_given, array_column($users, "id"));
+                                            
+                                            $assigned_user_key =  array_search($task->assignee, array_column($users, "id"));
+                                            $follow_user_key =  array_search($task->reporter, array_column($users, "id"));
+                                            
+                                            $start_date = date($this->config->item('date_format'), strtotime($task->start_date));
+                                            $end_date = date($this->config->item('date_format'), strtotime($task->end_date));
 
-											$task_title = strlen( $task->t_title ) > 25 ? substr($task->t_title , 0, 25) . "..." : $task->t_title;
+                                            $task_title = strlen($task->t_title) > 25 ? substr($task->t_title, 0, 25) . "..." : $task->t_title;
 
-											//task
-											echo '<tr>';
+                                            //task
+                                            echo '<tr>';
 
-											echo '<td><b>GEW</b>_<b>' . $users[$assigned_user_key]["username"] . "</b>_<b>" . $task->t_code . '</b>  </td>';
-											echo '<td>' . $task_title . '</td>';
+                                            echo '<td><b>GEW</b>_<b>' . $users[$assigned_user_key]["username"] . "</b>_<b>" . $task->t_code . '</b>  </td>';
+                                            echo '<td>' . $task_title . '</td>';
 
-											echo '<td>' . $users[$assigned_user_key]["first_name"] . " " . $users[$assigned_user_key]["last_name"] . '</td>';
+                                            echo '<td>' . $users[$assigned_user_key]["first_name"] . " " . $users[$assigned_user_key]["last_name"] . '</td>';
 
-											echo '<td> --- </td>';
+                                            echo '<td>' . $users[$given_by_key]["first_name"] . " " . $users[$given_by_key]["last_name"] . '</td>';
 
-											echo '<td>' . $task->c_name . '</td>';
-											echo '<td>' . $job_types[$task->parent_id] . '</td>';
+                                            echo '<td>' . $task->c_name . '</td>';
+                                            echo '<td>' . $job_types[$task->parent_id] . '</td>';
 
-											echo '<td> --- </td>';
+                                            echo '<td>' . $users[$follow_user_key]["first_name"] . " " . $users[$follow_user_key]["last_name"] . '</td>';
 
-											echo '<td>' . $start_date . '</td>';
-											echo '<td>' . $end_date . '</td>';
-											echo '<td>' . getStatusText($task->t_status) . '</td>';
-											?>
-											<td>
-												<a href="<?php echo base_url("/report/history/" . $task->tid); ?>" class="btn btn-info btn-sm">
-													View History
-												</a>
-											</td>
-										</tr>
-										<?php
-										}
-										?>
-								</tbody>
-							</table>
-						</div>
-					<?php else : ?>
-						<br>
-						<div class="col-md-4 offset-4">
-							<div class="alert alert-primary">
-								<span>
-									<b> Sorry!</b> There are no tasks under this job type
-								</span>
-							</div>
-						</div>
-					<?php endif; ?>
-				</div>
-			</div>
-		</div>
+                                            echo '<td>' . $start_date . '</td>';
+                                            echo '<td>' . $end_date . '</td>';
+                                            echo '<td>' . getStatusText($task->t_status) . '</td>';
+                                            ?>
+                                            <td>
+                                                <a href="<?php echo base_url("/report/history/" . $task->tid); ?>" class="btn btn-info btn-sm">
+                                                    View History
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                        ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    <?php else : ?>
+                        <br>
+                        <div class="col-md-4 offset-4">
+                            <div class="alert alert-primary">
+                                <span>
+                                    <b> Sorry!</b> There are no tasks under this job type
+                                </span>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
 
-	</div>
+    </div>
 
 
 </div>
@@ -114,40 +117,40 @@ echo '<td>' . $users[$given_by_key]["first_name"] . " " . $users[$given_by_key][
 
 echo '<td>' . $users[$given_by_key]["first_name"] . " " . $users[$given_by_key]["last_name"] . '</td>';
 
-	Dashboard for User body-->
+    Dashboard for User body-->
 
 
 
 
 <footer class="footer">
-	<!--	<div class="container-fluid">
-		<nav>
-			<ul>
-				<li>
-					<a href="http://www.gulfenviro.ae/">
-						GULF ENVIRONMENT & CO
-					</a>
-				</li>
-				<li>
-					<a href="http://www.gulfenviro.ae/">
-						News Alert
-					</a>
-				</li>
-				<li>
-					<a href="http://blog.creative-tim.com">
-						Blog
-					</a>
-				</li>
-			</ul>
-		</nav>
-		<div class="copyright" id="copyright">
-			&copy;
-			<script>
-				document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-			</script> Design & Code by
-			<a href="http://www.gulfenviro.ae/" target="_blank">Amir Nisar</a>.
-		</div>
-	</div>
+    <!--    <div class="container-fluid">
+        <nav>
+            <ul>
+                <li>
+                    <a href="http://www.gulfenviro.ae/">
+                        GULF ENVIRONMENT & CO
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.gulfenviro.ae/">
+                        News Alert
+                    </a>
+                </li>
+                <li>
+                    <a href="http://blog.creative-tim.com">
+                        Blog
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright" id="copyright">
+            &copy;
+            <script>
+                document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+            </script> Design & Code by
+            <a href="http://www.gulfenviro.ae/" target="_blank">Amir Nisar</a>.
+        </div>
+    </div>
 
 -->
 </footer>
